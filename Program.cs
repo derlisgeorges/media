87da7cdd07d162ecfbed2fa95d6ffb7df6f0d9af
Derlis <dsgezika@gmail.com>
2020-11-14 17:19:13 -0300

    media

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a primeira nota: ");
+            double nota1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a segunda nota: ");
+            double nota2 = double.Parse(Console.ReadLine());
+
+            double media = (nota1 + nota2) / 2;
+
+            string situacao = "";
+
+            Console.WriteLine("Media: " + media);
+
+            if(media > 7){
+                situacao = "Aprovado";
+            } else{
+                situacao = "Reprovado";
+            }
+
+            Console.WriteLine(situacao);
+            
+        }
+    }
+}
